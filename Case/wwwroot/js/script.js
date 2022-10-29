31491abd700e8b68afbf4704d3795a04f270dcee
omursnck <omrsnr12@icloud.com>
2022-10-29 19:42:32 +0300

    .NetCase

---
+++ Case/wwwroot/js/script.js
@@ -0,0 +1,24 @@
+﻿fetch("https://fakestoreapi.com/products").then((data) => {
+
+    return data.json();
+}).then((objectData) => {
+    console.log(objectData[0].title);
+    let tableData = "";
+    objectData.map((values) => {
+        tableData += ` <tr>
+             
+                <td>${values.title}</td>
+                <td>${values.description}</td>
+                <td>${values.price}</td>
+                <td><img src="${values.image}"/></td>
+              <td>  <button onclick="addToCart(${values.id})"; class="btn-primary";">Add to Cart</button></td>
+
+            </tr>`;
+    })
+    document.getElementById("table_body").innerHTML = tableData;
+}).catch((err) => {
+    console.log(err);
+})
+
+
+
